a712e6f16538cfed0b3f2fe7985ee53cd0e844d2
Guilherme Ziolle <gziolle@gmail.com>
2022-02-19 22:03:49 -0300

    move ruby install steps

--- install.sh
+++ install.sh
@@ -22,15 +22,6 @@ nvm install lts;
 nvm install lts/erbium;
 nvm install lts/fermium;
 
-echo "rvm - install\n";
-curl -L https://get.rvm.io | bash -s stable;
-
-echo "export necessary flags to install ruby\n";
-export warnflags=-Wno-error=implicit-function-declaration;
-
-echo "rvm install ruby-2.6.5\n";
-rvm install --default 2.6.5;
-
 echo "install java\n";
 brew install openjdk@11;
 
@@ -49,6 +40,16 @@ brew install neovim postman watchman heroku;
 echo "install casks\n";
 brew install --cask visual-studio-code flipper android-studio postman stats rectangle spotify slack zoom dbeaver-community google-chrome;
 
+echo "rvm - install\n";
+curl -L https://get.rvm.io | bash -s stable;
+
 echo "add rvm to path and source it\n";
 echo "export "HOME/.rvm/bin:$PATH"" >> ~/.zshrc;
 source ~/.zshrc;
+
+echo "export necessary flags to install ruby\n";
+export warnflags=-Wno-error=implicit-function-declaration;
+
+echo "rvm install ruby-2.6.5\n";
+rvm install --default 2.6.5;
+
